88db8fd1c5d0a47c9227a28f0342dca8bf81028b
uiofgh <2574127618@qq.com>
2025-04-01 00:36:58 +0800

    Add Demo

---
+++ client/app.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import dearpygui.dearpygui as dpg
+
+def save_callback():
+    print("Save Clicked")
+
+dpg.create_context()
+
+with dpg.font_registry():
+    with dpg.font("font/msyh.ttf", 20) as font1:
+
+        # add the default font range
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
+
+        # helper to add range of characters
+        #    Options:
+        #        mvFontRangeHint_Japanese
+        #        mvFontRangeHint_Korean
+        #        mvFontRangeHint_Chinese_Full
+        #        mvFontRangeHint_Chinese_Simplified_Common
+        #        mvFontRangeHint_Cyrillic
+        #        mvFontRangeHint_Thai
+        #        mvFontRangeHint_Vietnamese
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
+        dpg.bind_font(font1)
+
+dpg.create_viewport()
+dpg.setup_dearpygui()
+
+with dpg.window(label="Example Window"):
+    dpg.add_text("Hello world啊啊")
+    dpg.add_button(label="Save", callback=save_callback)
+    dpg.add_input_text(label="string")
+    dpg.add_slider_float(label="float")
+
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
